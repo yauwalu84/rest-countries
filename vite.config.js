9233513b7859815fd840ba671f5ec3d4f8df4c8f
yahaya.auwalu@azubiafrica.org <yahaya.auwalu@azubiafrica.org>
2024-08-13 20:00:07 +0100

    Edit vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/rest-countries/", // Replace with your repository name
   resolve: {
     alias: {
       "@components": "/src/components",
